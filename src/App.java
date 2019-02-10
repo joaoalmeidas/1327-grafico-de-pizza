cf18842e000837e27de52e712b41215756679516
joao <joao182cx@gmail.com>
2019-02-09 23:38:52 -0200

    Criação do layout do programa.

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class App {
+	
+	public static void main(String[] args) {
+		
+		GraficoFrame grafico = new GraficoFrame();
+		grafico.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		grafico.setSize(500, 500);
+		grafico.setVisible(true);
+		
+	}
+
+}
---
+++ src/GraficoFrame.java
@@ -0,0 +1,53 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class GraficoFrame extends JFrame{
+
+	private JTextField fieldAzul;
+	private JTextField fieldVermelho;
+	private JTextField fieldVerde;
+	private JTextField fieldAmarelo;
+	
+	private JLabel labelAzul;
+	private JLabel labelVermelho;
+	private JLabel labelVerde;
+	private JLabel labelAmarelo;
+	
+	private JPanel panelQuantidades;
+	private GraficoPanel panelGrafico;
+	
+	public GraficoFrame() {
+		
+		panelQuantidades = new JPanel(new FlowLayout());
+		panelGrafico = new GraficoPanel();
+		
+		fieldAzul = new JTextField(3);
+		fieldVermelho = new JTextField(3);
+		fieldVerde = new JTextField(3);
+		fieldAmarelo = new JTextField(3);
+		
+		labelAzul = new JLabel("Azul");
+		labelVermelho = new JLabel("Vermelho");
+		labelVerde = new JLabel("Verde");
+		labelAmarelo = new JLabel("Amarelo");
+		
+		panelQuantidades.add(labelAzul);
+		panelQuantidades.add(fieldAzul);
+		panelQuantidades.add(labelVermelho);
+		panelQuantidades.add(fieldVermelho);
+		panelQuantidades.add(labelVerde);
+		panelQuantidades.add(fieldVerde);
+		panelQuantidades.add(labelAmarelo);
+		panelQuantidades.add(fieldAmarelo);
+		
+		add(panelQuantidades, BorderLayout.NORTH);
+		add(panelGrafico);
+		
+	}
+	
+}
