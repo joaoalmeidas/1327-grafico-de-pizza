96927d2597fb5ccc39a9af1566a958495193ab18
joao <joao182cx@gmail.com>
2019-02-10 00:07:13 -0200

    Criação da representação grafica do gráfico em forma de pizza.

--- src/GraficoPanel.java
+++ src/GraficoPanel.java
@@ -1,7 +1,28 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Arc2D;
+
 import javax.swing.JPanel;
 
 public class GraficoPanel extends JPanel {
 	
-
+	public void paintComponent(Graphics g) {
+		
+		Graphics2D g2d = (Graphics2D) g;
+		
+		
+		g.setColor(Color.RED);
+		g2d.fill(new Arc2D.Double(getWidth()/4, getHeight()/4, getWidth()/2, getHeight()/2, 0, 90, Arc2D.PIE));
+		
+		g.setColor(Color.BLUE);
+		g2d.fill(new Arc2D.Double(getWidth()/4, getHeight()/4, getWidth()/2, getHeight()/2, 90, 90, Arc2D.PIE));
+		
+		g.setColor(Color.GREEN);
+		g2d.fill(new Arc2D.Double(getWidth()/4, getHeight()/4, getWidth()/2, getHeight()/2, 180, 90, Arc2D.PIE));
+		
+		g.setColor(Color.YELLOW);
+		g2d.fill(new Arc2D.Double(getWidth()/4, getHeight()/4, getWidth()/2, getHeight()/2, 270, 90, Arc2D.PIE));
+	}
 	
 }
